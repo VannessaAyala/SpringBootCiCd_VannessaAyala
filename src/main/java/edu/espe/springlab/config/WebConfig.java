8521ccd02282556e473f34cda1480c2c17f88bdc
VannessaAyala <vane992059@gmail.com>
2026-05-08 16:15:57 -0500

    Create test shouldNotAllowDuplicatedEmail

--- src/main/java/edu/espe/springlab/config/WebConfig.java
+++ src/main/java/edu/espe/springlab/config/WebConfig.java
@@ -5,8 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerxxx;
-
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
